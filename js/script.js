fc5ffa930cc474c17b0968d704f9e5b200b68eaf
AleksandraLomako <115226260+AleksandraLomako@users.noreply.github.com>
2022-10-25 13:04:31 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,35 @@ function test_btn() {
     doc.getElementById('scaner').innerHTML = ""
     document.getElementById('main_menu_wrapper').style.display = 'block'
 }
+
+function ScanerQR(scaner) {
+    var doc = document;
+    console.log(scaner);
+    doc.getElementById('scaner').innerHTML = ""
+    const newDiv = doc.createElement('div');
+    newDiv.id = 'scanerDiv';
+    newDiv.className = 'scanerDiv';
+
+    const newInput = doc.createElement('input');
+    newInput.type = 'text';
+    newInput.id = 'scaner_field';
+    newDiv.appendChild(newInput);
+    const currentDiv = doc.getElementById("scaner");
+    currentDiv.appendChild(newDiv);
+    doc.getElementById('scaner_field').focus();
+    checkValue()
+}
+
+function checkValue() {
+    var doc = document;
+    doc.getElementById('scaner_field').addEventListener("input", function () {
+        var x = doc.getElementById("scaner_field").value;
+        if (x.slice(-1) == " " || x.slice == "Space" || x.slice == 32) {
+            console.log('END');
+            console.log(x);
+
+            alert(x);
+            doc.getElementById('scaner_field').value = ''
+        }
+    });
+}
