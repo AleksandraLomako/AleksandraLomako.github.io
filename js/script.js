71c1d5aca7054488a05683777dc843340cf357c1
AleksandraLomako <115226260+AleksandraLomako@users.noreply.github.com>
2022-10-25 13:09:34 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,18 @@ function test_btn() {
     document.getElementById('main_menu_wrapper').style.display = 'block'
 }
 
-function ScanerQR(scaner) {
-    var doc = document;
-    alert(scaner);
+function ScanerQR() {
+   var doc = document;
+    doc.getElementById('scaner').innerHTML = ""
+    const newDiv = doc.createElement('div');
+    newDiv.id = 'scanerDiv';
+    newDiv.className = 'scanerDiv';
+
+    const newInput = doc.createElement('input');
+    newInput.type = 'text';
+    newInput.id = 'scaner_field';
+    newDiv.appendChild(newInput);
+    const currentDiv = doc.getElementById("scaner");
+    currentDiv.appendChild(newDiv);
+    doc.getElementById('scaner_field').focus();
 }
