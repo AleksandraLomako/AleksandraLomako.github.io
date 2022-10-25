dc3fa760eb168de3db1b9acee9a80fb4c0c2a194
AleksandraLomako <115226260+AleksandraLomako@users.noreply.github.com>
2022-10-25 12:52:20 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-
-
-
 function test() {
     alert('test')
+}
+
+function test_btn() {
+    let doc = document;
+    doc.getElementById('scaner').innerHTML = ""
+    doc.getElementById('main_menu_wrapper').style.display = 'block'
 }
